9ee8da9fc8cece0717d08876662d70b1261107d4
Cody Moniz <unyo@users.noreply.github.com>
2019-05-13 11:51:47 -1000

    Update WrapPageElement.js
    Updating WrapPageElement so it works with latest gatsby

--- src/components/WrapPageElement.js
+++ src/components/WrapPageElement.js
@@ -2,16 +2,23 @@ import React from "react";
 import posed, { PoseGroup } from "react-pose";
 
 import Layout from "./Layout";
-import { DefaultPose } from "./poses";
+import { DefaultPose, FromTop, CatTransition, FromRight } from "./poses";
 
 export const WrapPageElement = ({ element, props }) => {
-  const { key } = props.location;
-  const { pose, transitionComponent } = element.props;
+  const { key, pathname } = props.location;
+  // element.props doesn't provide defaultProps for some reason in latest gatsby
+  //const { pose, transitionComponent } = element.props;
+  
+  const routeToTransition = {
+    "/dog/": FromTop,
+    "/cat/": CatTransition,
+    "/": FromRight,
+  }
 
-  const Pose = pose ? posed.div(pose) : DefaultPose;
+  const Pose = routeToTransition[pathname]
 
-  const UsedTransition = transitionComponent || Pose;
-
+  const UsedTransition = Pose;
+ 
   return (
     <Layout>
       <PoseGroup>
